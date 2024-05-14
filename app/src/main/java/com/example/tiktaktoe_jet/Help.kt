6a79eb8c5200888e4227df048fd08fd9860a0056
Cesar2306 <79267531+Cesar2306@users.noreply.github.com>
2024-05-14 16:57:53 +0200

    navigation portrait/landscape done

--- app/src/main/java/com/example/tiktaktoe_jet/Help.kt
+++ app/src/main/java/com/example/tiktaktoe_jet/Help.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
