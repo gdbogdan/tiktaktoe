1d51e412fc77f643616ec5ff2dedb9a5f35bcf02
unknown <bogdangd94@gmail.com>
2024-05-29 22:16:23 +0200

    Pantalla de preparación scrollable además de botones en vertical

--- app/src/main/java/com/example/tiktaktoe_jet/PreparationGame.kt
+++ app/src/main/java/com/example/tiktaktoe_jet/PreparationGame.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -22,13 +24,12 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -45,15 +46,14 @@ private var difficulty: String = ""
 
 @Composable
 fun PreparationGame(navController: NavHostController, viewModel: GameViewModel = viewModel()) {
-    val context = LocalContext.current
+    val scrollState = rememberScrollState()
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.purple_200))
-    )
-    if (AppTheme.orientation == Orientation.Vertical) {
+    ) {
         Column(
-            modifier = Modifier.padding(top = 10.dp)
+            modifier = Modifier.padding(top = 10.dp).verticalScroll(scrollState)
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -146,15 +146,6 @@ fun PreparationGame(navController: NavHostController, viewModel: GameViewModel =
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Button(
-                        onClick = {navController.navigate("screen_menu") }
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.flecha_izquierda),
-                            contentDescription = null
-                        )
-                        Text(text = stringResource(id = R.string.Menu))
-                    }
-                    Button(
                         onClick = {
                             if (aliasText.isNotBlank()) {
                                 viewModel.setDifficulty(difficulty)
@@ -168,128 +159,25 @@ fun PreparationGame(navController: NavHostController, viewModel: GameViewModel =
                         Text(text = stringResource(id = R.string.Start))
                     }
                 }
-            }
-        }
-
-
-    } else {
-        Column(
-            modifier = Modifier.padding(top = 10.dp)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(bottom = 30.dp)
-            ) {
-                Image(painter = painterResource(id = R.drawable.ajuste), contentDescription = null)
-                Text(
-                    text = stringResource(id = R.string.Conf),
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 10.dp),
-
-
+                Row(
+                    modifier = Modifier
+                        .padding(top = 30.dp)
+                        .padding(start = 10.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                Text(text = stringResource(id = R.string.Alias), fontWeight = FontWeight.Bold)
-                var aliasText by rememberSaveable {
-                    mutableStateOf("")
-                }
-
-                TextField(
-                    value = aliasText,
-                    modifier = Modifier.padding(top = 10.dp),
-                    onValueChange = { aliasText = it },
-                    label = {
-                        Text(text = stringResource(id = R.string.HintAlias))
-                    },
-                    trailingIcon = {
-                        Icon(
-                            imageVector = Icons.Outlined.Person, contentDescription = null
+                    Button(
+                        onClick = {navController.navigate("screen_menu") }
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.flecha_izquierda),
+                            contentDescription = null
                         )
-                    },
-                    supportingText = {
-                        if (aliasText.isBlank()) {
-                            Text(
-                                text = stringResource(id = R.string.AliasError),
-                                color = colorResource(id = R.color.red)
-                            )
-                        }
+                        Text(text = stringResource(id = R.string.Menu))
                     }
-
-
-                )
-                Row(
-                    modifier = Modifier.padding(top = 10.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.graella),
-                        contentDescription = null,
-                        Modifier.size(25.dp)
-                    )
-                    Text(
-                        text = stringResource(id = R.string.Dificultad),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(start = 10.dp)
-                    )
                 }
-
-                RadioButtonFun()
-
-                Row(
-                    modifier = Modifier.padding(top = 10.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.reloj),
-                        contentDescription = null,
-                        Modifier.size(25.dp)
-                    )
-                    Text(
-                        text = stringResource(id = R.string.Tiempo),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .padding(start = 10.dp)
-                            .padding(end = 20.dp)
-                    )
-                    SwitchFun()
-
-                }
-                Button(
-                    onClick = { navController.navigate("screen_menu")}
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.flecha_izquierda),
-                        contentDescription = null
-                    )
-                    Text(text = stringResource(id = R.string.Menu))
-                }
-
-                Button(
-                    onClick = {
-                        if (aliasText.isNotBlank()) {
-                            viewModel.setDifficulty(difficulty)
-                            if (difficulty == "individual")
-                                navController.navigate("screen_game/$aliasText/$difficulty/$timer")
-                            if (difficulty == "multijugador")
-                                navController.navigate("screen_multigame/$aliasText/$difficulty/$timer")
-                        }
-                        else{
-                            Toast.makeText(context,R.string.AliasError, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                ) {
-                    Text(text = stringResource(id = R.string.Start))
-                }
-
             }
-
         }
+
 
     }
 }
